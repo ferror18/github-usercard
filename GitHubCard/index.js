f6cb432c6253241b1c4d316b288575b74dfc2533
manuel <me@example.com>
2020-05-07 18:57:02 -0700

    MVP Done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -53,6 +53,7 @@ followersArray.forEach(element => {
     
   })
 })
+//mvp done
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
